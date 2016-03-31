f5bcba42cf219fc4104d44ac4f78f1f56dcc7151
PetterLidbeck <b14petli@hs.local>
2016-03-31 17:12:03 +0200

    Animator Test 1
    Youtube tutorial 01-02

---
+++ Old World/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+public class Movement : MonoBehaviour {
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //turning
+        //jumping
+        //move
+        Move();
+	}
+
+    void Move()
+    {
+        anim.SetFloat("Forward", Input.GetAxisRaw("Vertical"));
+    }
+}

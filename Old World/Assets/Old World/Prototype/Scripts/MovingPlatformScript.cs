ac1bfca351f8692afd7c6ebd653f1ae3ba5a817f
Nerfertius <nerfertius@gmail.com>
2016-04-04 13:12:07 +0200

    Elevator Script & Binary fix
    Created script for elevator platform
    Changed from binary coding to text

---
+++ Old World/Assets/Old World/Prototype/Scripts/MovingPlatformScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatformScript : MonoBehaviour
+{
+    private Vector3 StartTransform;
+    private bool MoveToTarget;
+
+    public Vector3 TargetTransform;
+    public int Speed;
+    
+    void Start ()
+    {
+        MoveToTarget = true;
+        StartTransform = gameObject.transform.position;
+	}
+	
+	void Update ()
+    {
+        if (MoveToTarget == true)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, TargetTransform, Speed * Time.deltaTime);
+            if (gameObject.transform.position == TargetTransform)
+            {
+                MoveToTarget = false;
+            }
+        }
+
+        if (MoveToTarget == false)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, StartTransform, Speed * Time.deltaTime);
+            if (gameObject.transform.position == StartTransform)
+            {
+                MoveToTarget = true;
+            }
+        }
+	}
+}

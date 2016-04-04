196e84f991b6b3846d587c22a1d10f157c0d1fbd
Nerfertius <nerfertius@gmail.com>
2016-04-04 11:13:44 +0200

    Room 1.1

---
+++ Old World/Assets/LightScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightScript : MonoBehaviour
+{
+    public Light lgt;
+
+    private bool LightSwitch;
+
+	void Start ()
+    {
+        lgt = GetComponent<Light>();
+
+        LightSwitch = false;
+
+    }
+	
+	void Update ()
+    {
+        if (LightSwitch == false)
+        {
+            lgt.intensity += 2 * Time.deltaTime;
+            if (lgt.intensity >= 8)
+            {
+                LightSwitch = true;
+            }
+        }
+        if (LightSwitch == true)
+        {
+            lgt.intensity -= 2F * Time.deltaTime;
+            if (lgt.intensity < 3)
+            {
+                LightSwitch = false;
+            }
+        }
+    }
+}

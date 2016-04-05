f025e0a9f61202d63f077bdffc39cb2df02791f1
Twilice <molkiar@gmail.com>
2016-04-05 15:36:31 +0200

    superbasic reflection

---
+++ Old World/Assets/PrismReflect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrismReflect : MonoBehaviour {
+
+    GameObject spotlight;
+
+    void Start()
+    {
+        foreach(Transform go in gameObject.GetComponentsInChildren<Transform>(true))
+            if (go.tag.Equals("LightShaft"))
+                spotlight = go.gameObject;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("LightShaft"))
+            spotlight.SetActive(true);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("LightShaft"))
+            spotlight.SetActive(false);
+    }
+
+}

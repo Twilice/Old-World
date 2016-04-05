47bb03d6deb521b74f88443295d25c9062ce1df6
Nerfertius <nerfertius@gmail.com>
2016-04-05 13:11:31 +0200

    Room 1-3, Materials, Prefabs
    Room 1-3 started
    Materials for Battery, Charger and Generator
    Started fix on MovingPlatform

--- Old World/Assets/Old World/Prototype/Scripts/MovingPlatformScript.cs
+++ Old World/Assets/Old World/Prototype/Scripts/MovingPlatformScript.cs
@@ -4,22 +4,32 @@ using System.Collections;
 public class MovingPlatformScript : MonoBehaviour
 {
     private Vector3 StartTransform;
+    private Quaternion StartRotation;
     private bool MoveToTarget;
 
     public Vector3 TargetTransform;
+    public Quaternion TargetRotation;
+    public Transform Target;
     public int Speed;
+    public bool Rotate;
     
     void Start ()
     {
         MoveToTarget = true;
         StartTransform = gameObject.transform.position;
-	}
+        StartRotation = gameObject.transform.rotation;
+
+    }
 	
 	void Update ()
     {
         if (MoveToTarget == true)
         {
             gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, TargetTransform, Speed * Time.deltaTime);
+            if (Rotate == true)
+            {
+                //gameObject.transform.rotation = Vector3.RotateTowards(gameObject.transform.rotation, TargetRotation, Speed * Time.deltaTime, 0.0F);
+            }
             if (gameObject.transform.position == TargetTransform)
             {
                 MoveToTarget = false;

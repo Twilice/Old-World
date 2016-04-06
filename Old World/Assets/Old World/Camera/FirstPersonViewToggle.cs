1d4f684f1d82ec3e985d6afe18d8b1872f7a5b5a
PetterLidbeck <b14petli@hs.local>
2016-04-06 14:22:35 +0200

    ShoulderViewToggleStart

---
+++ Old World/Assets/Old World/Camera/FirstPersonViewToggle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class FirstPersonViewToggle : MonoBehaviour {
+    private MouseOrbitImproved moi;
+    private ThirdPersonCharacter tpc;
+    private ThirdPersonUserControl tpuc;
+    private RigidbodyFirstPersonController rbfpc;
+    // Use this for initialization
+    void Start () {
+        
+        moi = GetComponent<MouseOrbitImproved>();
+        moi.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(1))
+        {
+            moi.enabled = false;
+            
+        }
+	}
+}

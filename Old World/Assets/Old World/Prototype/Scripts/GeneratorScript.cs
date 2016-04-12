b9ba24972404f6e403259ade175df018cf04df7a
Nerfertius <nerfertius@gmail.com>
2016-04-12 16:30:46 +0200

    Added activator scripts
    Created and finished script for solar panels (needs the TriggeredByLight
    base class). Updated prefab with script.
    Created and finished script for Generator (needs the TriggeredByLight
    base class). Updated prefab with script.
    Created script for charging station, but didn't start any work on it.

---
+++ Old World/Assets/Old World/Prototype/Scripts/GeneratorScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneratorScript : MonoBehaviour
+{
+	public List<GameObject> Targets;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		for (int i = 0; i < Targets.Count; i++)
+		{
+			Targets[i].GetComponent<MovingPlatformScript>().enabled = true;
+			Targets[i].GetComponent<GeneratorScript>().enabled = true;
+			Targets[i].GetComponent<ChargerScript>().enabled = true;
+		}
+	}
+}
---
+++ Old World/Assets/Old World/Prototype/Scripts/SolarPanel.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SolarPanel : MonoBehaviour//TriggeredByLight
+{
+	public List<GameObject> Targets;
+	public int ChargeUpTime;
+
+	private bool Active;
+
+	void Start ()
+	{
+		Active = false;
+    }
+	
+	void Update ()
+	{
+		if (Active == true)
+		{
+			//Light on panel turning on?
+		}
+	}
+
+	void HitByLightStay()
+	{
+		//if (timeIlluminated >= ChargeUpTime)
+		//{
+			//Play sound once
+			Active = true;
+			for (int i = 0; i < Targets.Count; i++)
+			{
+				Targets[i].GetComponent<MovingPlatformScript>().enabled = true;
+				Targets[i].GetComponent<GeneratorScript>().enabled = true;
+				Targets[i].GetComponent<ChargerScript>().enabled = true;
+			}
+		//}
+
+		//else
+		//{
+		//	//Play powering up sound
+		//	//Fade lights on?
+		//}
+	}
+}

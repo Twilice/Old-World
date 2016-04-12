17eace7ba5a77b50cea37bf373fb8896e6a86bf3
PetterLidbeck <b14petli@hs.local>
2016-04-12 11:12:48 +0200

    Light looks in the right direction + sphere lens

--- Old World/Assets/Old World/Scripts/LensReflect.cs
+++ Old World/Assets/Old World/Scripts/LensReflect.cs
@@ -1,25 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PrismReflect : MonoBehaviour {
+public class LensReflect : MonoBehaviour {
 
     GameObject spotlight;
+    private Animator anim;
+    private bool inLight;
+
+    void Awake()
+    {
+        anim = GameObject.Find("Player").GetComponent<Animator>();
+    }
 
     void Start()
     {
         foreach(Transform go in gameObject.GetComponentsInChildren<Transform>(true))
             if (go.tag.Equals("LightReflected"))
                 spotlight = go.gameObject;
+
+        inLight = false;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag.Equals("LightShaft"))
-            spotlight.SetActive(true);
+        if (other.tag.Equals("LightShaft"))
+            inLight = true;
     }
     void OnTriggerExit(Collider other)
     {
         if (other.tag.Equals("LightShaft"))
+            inLight = false;
+    }
+
+    void Update()
+    {
+        if (inLight && anim.GetBool("firstPerson"))
+            spotlight.SetActive(true);
+        else
             spotlight.SetActive(false);
     }
 
---
+++ Old World/Assets/Old World/Scripts/MoveLensTarget.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLensTarget : MonoBehaviour {
+
+    private Transform target;
+    private Animator anim;
+    private Transform lens;
+    private MeshRenderer lensMesh;
+
+    void Awake()
+    {
+        target = GameObject.Find("LensTarget").transform;
+        anim = GameObject.Find("Player").GetComponent<Animator>();
+        lens = GameObject.Find("Player/Lens/ReflectedLensLight").transform;
+        lensMesh = GameObject.Find("Player/Lens").GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (anim.GetBool("firstPerson"))
+        {
+            //Show the lens
+            lensMesh.enabled = true;
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 500))
+            {
+                target.position = hit.point;
+            }
+            else
+            {
+                target.position = transform.position + transform.TransformDirection(Vector3.forward) * 10;
+            }
+            lens.LookAt(target);
+        }
+        else
+        {
+            //Hide the lens
+            lensMesh.enabled = false;
+        }
+    }
+}

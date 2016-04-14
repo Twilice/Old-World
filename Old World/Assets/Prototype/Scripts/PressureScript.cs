c3545bb16d639ab14ae97e1321384e780e8e1732
Nerfertius <nerfertius@gmail.com>
2016-04-14 17:44:26 +0200

    Room 1-1-5 mechanical done & pressure script
    All the mechanics for 1-1-5 is done, ready for testing
    
    Created a script for pressure plates & prefabb for the object

---
+++ Old World/Assets/Prototype/Scripts/PressureScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PressureScript : MonoBehaviour
+{
+	public List<GameObject> Targets;
+	
+	public bool PlatformTarget;
+	public bool GeneratorTarget;
+	public bool ChargerTarget;
+
+	void Start ()
+	{
+	}
+	
+	void Update ()
+	{
+	}
+
+	void OnTriggerStay(Collider coll)
+	{
+		if (coll.gameObject.CompareTag("Player") || coll.gameObject.CompareTag("Lens"))
+		{
+			for (int i = 0; i < Targets.Count; i++)
+			{
+				if (PlatformTarget == true)
+				{
+					Targets[i].GetComponent<MovingPlatformScript>().Activate();
+				}
+				if (GeneratorTarget == true)
+				{
+					Targets[i].GetComponent<GeneratorScript>().Activate();
+				}
+				//if (ChargerTarget == true)
+				//{
+				//	Targets[i].GetComponent<ChargerScript>().Activate();
+				//}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider coll)
+	{
+		if (coll.gameObject.CompareTag("Player") || coll.gameObject.CompareTag("Lens"))
+        {
+			for (int i = 0; i < Targets.Count; i++)
+			{
+				if (PlatformTarget == true)
+				{
+					Targets[i].GetComponent<MovingPlatformScript>().enabled = false;
+				}
+				//if (ChargerTarget == true)
+				//{
+				//	Targets[i].GetComponent<ChargerScript>().enabled = false;
+				//}
+			}
+		}
+		
+	}
+}

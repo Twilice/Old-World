0434b6b31ff4c7fda10e9ed44091b46e6f772677
Nerfertius <nerfertius@gmail.com>
2016-04-21 11:58:07 +0200

    Updated Battery script
    Can now be picked up and put into a charging station (press E)

--- Old World/Assets/Old World/Scripts/Battery.cs
+++ Old World/Assets/Old World/Scripts/Battery.cs
@@ -21,8 +21,26 @@ public class Battery : MonoBehaviour
 
     }
     
-    void OnCollisionEnter()
+    void OnTriggerStay(Collider coll)
     {
-		
+		if (coll.gameObject.CompareTag("Player"))
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				gameObject.transform.SetParent(coll.gameObject.transform);
+				gameObject.transform.localPosition = new Vector3(0, 1, 1);
+			}
+		}
+
+		if (coll.gameObject.CompareTag("Charger"))
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				gameObject.transform.SetParent(coll.gameObject.transform);
+				gameObject.transform.localPosition = new Vector3(0, 0, 0.5F);
+				gameObject.transform.localRotation = new Quaternion(0, 0, 0, 0);
+				gameObject.transform.localScale = new Vector3(0.8F, 0.8F, 1.5F);
+			}
+		}
     }
 }

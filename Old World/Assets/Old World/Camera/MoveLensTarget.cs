d4ea051bf982a60b6d0cdef4ab817af4a176b6bd
LucksterGaming <b14davjo@hs.local>
2016-04-27 11:31:40 +0200

    mergeresync

--- Old World/Assets/Old World/Camera/MoveLensTarget.cs
+++ Old World/Assets/Old World/Camera/MoveLensTarget.cs
@@ -61,6 +61,7 @@ public class MoveLensTarget : MonoBehaviour
                         target.position = transform.position + transform.TransformDirection(Vector3.forward) * 10;
                     }
                     lensLight.LookAt(target);
+                    lens.LookAt(target);
                 }
 
                 //Check if lens is dropped

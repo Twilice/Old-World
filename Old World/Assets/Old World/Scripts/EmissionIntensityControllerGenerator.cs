c7ca84d5739088e85d0981e5893392f5f1728762
PJarcade <c14antma@hs.local>
2016-04-29 16:22:01 +0200

    FMOD fix, plz git god

--- Old World/Assets/Old World/Scripts/EmissionIntensityControllerGenerator.cs
+++ Old World/Assets/Old World/Scripts/EmissionIntensityControllerGenerator.cs
@@ -5,66 +5,71 @@ using System.Collections;
 public class EmissionIntensityControllerGenerator : MonoBehaviour
 {
 
-    [Range(0, 5)]
-    public float maxIntensity = 2.7f;
-    [Range(1, 9)]
-    public int lightNumber;
+	[Range(0, 5)]
+	public float maxIntensity = 2.7f;
+	[Range(1, 9)]
+	public int lightNumber;
 
-    private Renderer r;
-    private float emissionIntensity = 0f;
-    private Color c;
-    private MeshRenderer mr;
-    private float energy = 0.0f;
-    private bool activated = false;
 
-    void Awake()
-    {
-        r = GetComponent<Renderer>();
-        mr = GetComponent<MeshRenderer>();
-        if (lightNumber == 9)
-        {
-            c = Color.red;
-            energy = 1.0f;
-        }
-        else
-        {
-            c = Color.white;
-        }
-    }
+	private EventPlayer sfx;
+	private Renderer r;
+	private float emissionIntensity = 0f;
+	private Color c;
+	private MeshRenderer mr;
+	private float energy = 0.0f;
+	private bool activated = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        emissionIntensity = maxIntensity * energy;
-        if (RoomState.roomFullyPowered && lightNumber == 9)
-        {
-            mr.material.SetColor("_EmissionColor", Color.green * emissionIntensity / 2f);
-        }
-        mr.material.SetColor("_EmissionColor", c * emissionIntensity / 2.0f);
-        DynamicGI.SetEmissive(r, c * emissionIntensity);
-    }
+	void Awake()
+	{
+		sfx = GetComponent<EventPlayer>();
+		r = GetComponent<Renderer>();
+		mr = GetComponent<MeshRenderer>();
+		if (lightNumber == 9)
+		{
+			c = Color.red;
+			energy = 1.0f;
+		}
+		else
+		{
+			c = Color.white;
+		}
+	}
 
-    //When the solarpanel is hit
-    public void LerpEnergy(float solarPanelEnergy)
-    {
-        if (solarPanelEnergy == 1.0f)
-        {
-            activated = true;
-            c = Color.green;
-        }
-        if (!activated)
-        {
-            if (lightNumber < 9)
-            {
-                if (solarPanelEnergy >= (float)lightNumber / 9.0f)
-                {
-                    energy = 1f;
-                }
-                else
-                {
-                    energy = 0f;
-                }
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		emissionIntensity = maxIntensity * energy;
+		if (RoomState.roomFullyPowered && lightNumber == 9)
+		{
+			mr.material.SetColor("_EmissionColor", Color.green * emissionIntensity / 2f);
+		}
+		mr.material.SetColor("_EmissionColor", c * emissionIntensity / 2.0f);
+		DynamicGI.SetEmissive(r, c * emissionIntensity);
+	}
+
+	//When the solarpanel is hit
+	public void LerpEnergy(float solarPanelEnergy)
+	{
+		if (solarPanelEnergy == 1.0f)
+		{
+			activated = true;
+			if (sfx != null)
+				sfx.PlayEvent();
+			c = Color.green;
+		}
+		if (!activated)
+		{
+			if (lightNumber < 9)
+			{
+				if (solarPanelEnergy >= (float)lightNumber / 9.0f)
+				{
+					energy = 1f;
+				}
+				else
+				{
+					energy = 0f;
+				}
+			}
+		}
+	}
 }

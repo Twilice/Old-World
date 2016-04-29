0356f1500cb47b1119b079460330b164d8147fa9
LucksterGaming <b14davjo@hs.local>
2016-04-29 16:57:52 +0200

    Lens animation almost done

---
+++ Old World/Assets/Old World/Scripts/LensAnimations.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LensAnimations : MonoBehaviour
+{
+    private SkinnedMeshRenderer smr;
+    private MoveLensTarget mlt;
+
+    private float value = 0.0f;
+    bool open = false;
+    bool idleDone = false;
+
+    void Start()
+    {
+        smr = GetComponent<SkinnedMeshRenderer>();
+        mlt = GameObject.Find("_Camera/MainCamera").GetComponent<MoveLensTarget>();
+    }
+
+    void Update()
+    {
+        if (mlt.LensActivated)
+        {
+            if (!open)
+            {
+                if (value <= 100)
+                {
+                    value += Time.deltaTime * 120;
+                    smr.SetBlendShapeWeight(1, value);
+                }
+                else
+                {
+                    open = true;
+                }
+            }
+            else if (open)
+            {
+                if (idleDone)
+                {
+                    if (value >= 50)
+                    {
+                        value -= Time.deltaTime * 60;
+                        smr.SetBlendShapeWeight(1, value);
+                    }
+                    else
+                    {
+                        idleDone = false;
+                    }
+                }
+                else if (!idleDone)
+                {
+                    if (value <= 100)
+                    {
+                        value += Time.deltaTime * 60;
+                        smr.SetBlendShapeWeight(1, value);
+                    }
+                    else
+                    {
+                        idleDone = true;
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            open = false;
+            value = 0;
+            smr.SetBlendShapeWeight(1, 0);
+            idleDone = true;
+        }
+    }
+}

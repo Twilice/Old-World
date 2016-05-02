3b2abfd206e595be266663cdc5eaf43891a1b16b
LucksterGaming <b14davjo@hs.local>
2016-05-02 16:39:06 +0200

    Lens anim lerp started

--- Old World/Assets/Old World/Player/LensAnimations.cs
+++ Old World/Assets/Old World/Player/LensAnimations.cs
@@ -7,8 +7,8 @@ public class LensAnimations : MonoBehaviour
     private MoveLensTarget mlt;
 
     private float value = 0.0f;
-    bool open = false;
-    bool idleDone = false;
+    private bool open = false;
+    private bool idleDone = false;
 
     void Start()
     {
@@ -18,12 +18,15 @@ public class LensAnimations : MonoBehaviour
 
     void Update()
     {
+        //Check if lens is activated
         if (mlt.LensActivated)
         {
+            //Checks if lens is fully opened in the animation
             if (!open)
             {
                 if (value <= 100)
                 {
+                    //Opens the lens for the first time
                     value += Time.deltaTime * 120;
                     smr.SetBlendShapeWeight(1, value);
                 }
@@ -34,10 +37,12 @@ public class LensAnimations : MonoBehaviour
             }
             else if (open)
             {
+                //Checks if idle anim is done
                 if (idleDone)
                 {
                     if (value >= 50)
                     {
+                        //Closes the lens
                         value -= Time.deltaTime * 60;
                         smr.SetBlendShapeWeight(1, value);
                     }
@@ -46,10 +51,12 @@ public class LensAnimations : MonoBehaviour
                         idleDone = false;
                     }
                 }
+                //Checks if it should start the idle anim
                 else if (!idleDone)
                 {
                     if (value <= 100)
                     {
+                        //Opens lens
                         value += Time.deltaTime * 60;
                         smr.SetBlendShapeWeight(1, value);
                     }
@@ -63,6 +70,7 @@ public class LensAnimations : MonoBehaviour
         }
         else
         {
+            //Resets the anim once the orb is gone
             open = false;
             value = 0;
             smr.SetBlendShapeWeight(1, 0);

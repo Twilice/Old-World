77a865dacb0440f5002c35dee4a162fd86dc3f8f
PetterLidbeck <b14petli@hs.local>
2016-05-03 15:08:54 +0200

    Total Makeover

---
+++ Old World/Assets/Old World/Props and Mechanics/Door and Generator/Scripts/GeneratorScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[DisallowMultipleComponent]
+public class GeneratorScript : MonoBehaviour
+{
+    public List<GameObject> Targets;
+    public bool PlatformTarget;
+    public bool GeneratorTarget;
+    public bool ChargerTarget;
+    public bool Active = false;
+
+    private DoorLight doorLight;
+    private DoorLight[] allDoorLights;
+    private MovingPlatformScript[] mps;
+    private List<MovingPlatformScript> targetPlatforms = new List<MovingPlatformScript>();
+    private ChargerScript[] cs;
+    private List<ChargerScript> targetChargers = new List<ChargerScript>();
+
+    void Awake()
+    {
+        allDoorLights = FindObjectsOfType<DoorLight>();
+        mps = FindObjectsOfType<MovingPlatformScript>();
+        cs = FindObjectsOfType<ChargerScript>();
+
+        //The doorLight with the same tag
+        for (int i = 0; i < allDoorLights.Length; i++)
+        {
+			if (allDoorLights[i].transform.CompareTag(transform.tag))
+            {
+                doorLight = allDoorLights[i];
+            }
+        }
+
+        //The movingPlatforms with the same tag
+        for(int i = 0; i < mps.Length; i++)
+        {
+            if (mps[i].transform.CompareTag(transform.tag))
+            {
+                targetPlatforms.Add(mps[i]);
+            }
+        }
+
+        //The chargerScripts with the same tag
+        for (int i = 0; i < cs.Length; i++)
+        {
+            if (cs[i].transform.CompareTag(transform.tag))
+            {
+                targetChargers.Add(cs[i]);
+            }
+        }
+    }
+
+    void Update()
+    {
+		if (Active == true)
+        {
+            //Activate the platforms
+            for(int i = 0; i < targetPlatforms.Count; i++)
+            {
+                targetPlatforms[i].Activate();
+                targetPlatforms[i].returning = false;
+            }
+
+            //Activate the chargers
+            for(int i = 0; i < targetChargers.Count; i++)
+            {
+                targetChargers[i].Activate();
+            }
+		}
+    }
+
+    public void Activate()
+    {
+        //Change bool to true
+        Active = true;
+
+		//Change the light
+		doorLight.Activate();
+    }
+}

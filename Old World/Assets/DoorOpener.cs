70b48376713fc5ace3a4a82d1982e1f505ceb359
PJarcade <c14antma@hs.local>
2016-05-03 15:35:49 +0200

    DoorOpener

---
+++ Old World/Assets/DoorOpener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			//Open door
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			//Close door
+		}
+	}
+}

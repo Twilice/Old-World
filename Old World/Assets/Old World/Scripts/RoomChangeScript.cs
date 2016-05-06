0774925c5166b79cd5ba59f682324575029a1078
Nerfertius <nerfertius@gmail.com>
2016-05-06 16:43:47 +0200

    Created script for switching rooms & logo
    WE'VE GOT A LOGO
    
    Also, the script is mostly for testing/debugging purposes.
    Tab: switch selection of scenes. Return: Load the selected scene.

---
+++ Old World/Assets/Old World/Scripts/RoomChangeScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class RoomChangeScript : MonoBehaviour
+{
+	private int counter = 0;
+	private Scene[] rooms;
+	
+	void Awake ()
+	{
+		rooms = SceneManager.GetAllScenes();
+		Debug.Log(rooms[counter].ToString());
+		Debug.Log(rooms.Length);
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			counter++;
+			if (counter > rooms.Length)
+			{
+				counter = 0;
+			}
+			Debug.Log(counter);
+			Debug.Log(rooms[counter].ToString());
+			
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			SceneManager.LoadScene(rooms[counter].ToString());
+		}
+	}
+}

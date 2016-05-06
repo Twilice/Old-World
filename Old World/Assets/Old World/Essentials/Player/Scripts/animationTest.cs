fdeef20c367fb10b154e51c81eb94b2a6706a853
Twilice <molkiar@gmail.com>
2016-05-06 16:15:56 +0200

    FlaviaModelStart!!

---
+++ Old World/Assets/Old World/Essentials/Player/Scripts/animationTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationTest : MonoBehaviour {
+
+    Animator m_Animator;
+    bool FPV = false;
+    // Use this for initialization
+    void Start () {
+
+        m_Animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_Animator.SetFloat("Forward", Input.GetAxis("Vertical"), 0.1f, Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            FPV = !FPV;
+            m_Animator.SetBool("FirstPerson", FPV);
+        }
+    }
+}

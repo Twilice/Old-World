c04977164715a02878661b3ef649ff558658c77b
Twilice <molkiar@gmail.com>
2016-05-09 16:44:07 +0200

    Ny playercontroller utan rootmotion på G

--- Old World/Assets/Old World/Essentials/Camera/ScriptsOnPlayer/RaycastCamera.cs
+++ Old World/Assets/Old World/Essentials/Camera/ScriptsOnPlayer/RaycastCamera.cs
@@ -7,34 +7,36 @@ public class RaycastCamera : MonoBehaviour
     public static float maxDistance = 8;
     void Update()
     {
+        int layerMask = 1 << LayerMask.NameToLayer("Player");
+        layerMask = ~layerMask;
         RaycastHit hit;
-        Vector3 topRightCorner = transform.position + transform.up *0.5f + transform.right * 0.5f;
-        Vector3 bottomLeftCorner = transform.position - transform.up * 0.5f - transform.right * 0.5f;
-        Vector3 bottomRightCorner = transform.position - transform.up * 0.5f + transform.right * 0.5f;
-        Vector3 topLeftCorner = transform.position + transform.up * 0.5f - transform.right * 0.5f;
+        Vector3 topRightCorner = transform.position + transform.up *0.3f + transform.right * 0.3f;
+        Vector3 bottomLeftCorner = transform.position - transform.up * 0.3f - transform.right * 0.3f;
+        Vector3 bottomRightCorner = transform.position - transform.up * 0.3f + transform.right * 0.3f;
+        Vector3 topLeftCorner = transform.position + transform.up * 0.3f - transform.right * 0.3f;
         float distance = maxDistance;
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, ~0, QueryTriggerInteraction.Ignore))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, layerMask, QueryTriggerInteraction.Ignore))
         {
             distance = Mathf.Min(distance, hit.distance);
         }
 
-        if (Physics.Raycast(topRightCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, ~0, QueryTriggerInteraction.Ignore))
+        if (Physics.Raycast(topRightCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, layerMask, QueryTriggerInteraction.Ignore))
         {
             distance = Mathf.Min(distance, hit.distance);
         }
 
-        if (Physics.Raycast(bottomLeftCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, ~0, QueryTriggerInteraction.Ignore))
+        if (Physics.Raycast(bottomLeftCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, layerMask, QueryTriggerInteraction.Ignore))
         {
             distance = Mathf.Min(distance, hit.distance);
         }
 
-        if (Physics.Raycast(bottomRightCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, ~0, QueryTriggerInteraction.Ignore))
+        if (Physics.Raycast(bottomRightCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, layerMask, QueryTriggerInteraction.Ignore))
         {
             distance = Mathf.Min(distance, hit.distance);
         }
 
-        if (Physics.Raycast(topLeftCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, ~0, QueryTriggerInteraction.Ignore))
+        if (Physics.Raycast(topLeftCorner, transform.TransformDirection(Vector3.forward), out hit, RaycastCamera.maxDistance, layerMask, QueryTriggerInteraction.Ignore))
         {
             distance = Mathf.Min(distance, hit.distance);
         }

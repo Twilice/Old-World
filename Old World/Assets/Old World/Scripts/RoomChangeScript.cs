72166bbb9587674d4c39944374957f63dc744502
Nerfertius <nerfertius@gmail.com>
2016-05-09 15:20:15 +0200

    Finished script for switching rooms
    TEST VERSION
    Currently you press TAB to cycle through which room you want to select,
    then press ENTER to load that scene

--- Old World/Assets/Old World/Scripts/RoomChangeScript.cs
+++ Old World/Assets/Old World/Scripts/RoomChangeScript.cs
@@ -5,13 +5,12 @@ using System.Collections;
 public class RoomChangeScript : MonoBehaviour
 {
 	private int counter = 0;
-	private Scene[] rooms;
+	private int numberOfRooms = 0;
 	
 	void Awake ()
 	{
-		rooms = SceneManager.GetAllScenes();
-		Debug.Log(rooms[counter].ToString());
-		Debug.Log(rooms.Length);
+		numberOfRooms = SceneManager.sceneCountInBuildSettings;
+		Debug.Log(counter);
 	}
 	
 	void Update ()
@@ -19,18 +18,17 @@ public class RoomChangeScript : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.Tab))
 		{
 			counter++;
-			if (counter > rooms.Length)
+			if (counter > numberOfRooms)
 			{
 				counter = 0;
 			}
 			Debug.Log(counter);
-			Debug.Log(rooms[counter].ToString());
-			
 		}
 
 		if (Input.GetKeyDown(KeyCode.Return))
 		{
-			SceneManager.LoadScene(rooms[counter].ToString());
+			SceneManager.LoadScene(counter, LoadSceneMode.Single);
+			Debug.Log("Loaded room: " + counter);
 		}
 	}
 }

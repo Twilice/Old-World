391d471f5974250c70f9062454876937215babb8
PJarcade <c14antma@hs.local>
2016-05-13 16:28:48 +0200

    main menu
    saknar slutgiltiga texturer

---
+++ Old World/Assets/MouseHover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnMouseEnter()
+	{
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+}

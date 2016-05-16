df96bc87f17e49e6e7d8e48b4030a59f02f74124
Nerfertius <nerfertius@gmail.com>
2016-05-16 11:10:10 +0200

    Started on buttonScript & changed door_o

---
+++ Old World/Assets/Old World/Scripts/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonScript : MonoBehaviour
+{
+	public List<GameObject> Targets;
+	public bool PlatformTarget;
+	public bool GeneratorTarget;
+	public bool ChargerTarget;
+	public bool Active = false;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}

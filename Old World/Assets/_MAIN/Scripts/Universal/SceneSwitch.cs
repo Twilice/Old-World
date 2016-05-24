25c5dbf5b32956294aa0767ebe054a2d94816a49
Twilice <molkiar@gmail.com>
2016-05-24 14:32:36 +0200

    flyttade sceneswitchcollider

--- Old World/Assets/_MAIN/Scripts/Universal/SceneSwitch.cs
+++ Old World/Assets/_MAIN/Scripts/Universal/SceneSwitch.cs
@@ -6,7 +6,7 @@ public class SceneSwitch : MonoBehaviour {
 
     public Rooms newScene = Rooms.Room1_2;
 
-    public void OnTriggerExit(Collider col)
+    public void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Player"))
         {

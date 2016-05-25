f3cb8c2b85864f75921a76f68d28c27df1b4fa58
Nerfertius <nerfertius@gmail.com>
2016-05-25 14:27:16 +0200

    Updated how to play
    HTP is now part of the pause menu prefab.

---
+++ Old World/Assets/HowToPlayScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HowToPlayScript : MonoBehaviour
+{
+	public List<GameObject> HTP_Objects;
+	
+	private bool active = false;
+
+	void Awake()
+	{
+		for (int i = 0; i < HTP_Objects.Count; i++)
+		{
+			HTP_Objects[i].SetActive(false);
+		}
+	}
+	
+	public void Activate()
+	{
+		if (active == false)
+		{
+			for (int i = 0; i < HTP_Objects.Count; i++)
+			{
+				HTP_Objects[i].SetActive(true);
+			}
+			active = true;
+		}
+		else
+		{
+			for (int i = 0; i < HTP_Objects.Count; i++)
+			{
+				HTP_Objects[i].SetActive(false);
+			}
+			active = false;
+		}
+	}
+}

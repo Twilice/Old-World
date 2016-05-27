757846f8a303d743ddf83ed364262219d10fcd5a
Twilice <molkiar@gmail.com>
2016-05-27 17:13:18 +0200

    7 Very important fixes

---
+++ Old World/Assets/Debug_Script_2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debug_Script_2 : MonoBehaviour
+{
+    private Renderer ghost;
+    private AudioSource flaviaNoise;
+    public MovieTexture megaShader;
+    
+	void Awake ()
+    {
+        flaviaNoise = GetComponent<AudioSource>();
+        ghost = gameObject.GetComponent<Renderer>();
+        ghost.material.mainTexture = megaShader;
+        megaShader.loop = true;
+        flaviaNoise.clip = megaShader.audioClip;
+	}
+	
+    void OnTriggerEnter()
+    {
+        megaShader.Play();
+        flaviaNoise.Play();
+    }
+}

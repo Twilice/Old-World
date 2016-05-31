b5941432b0b4fc9d6888fbe1e082472e1bfb37b0
Twilice <molkiar@gmail.com>
2016-05-31 16:48:04 +0200

    sourcekåd för repeater

---
+++ Old World/ProgramRepeater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StartProgramTestCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = Process.Start(@"Old_World.exe");
+            while (true)
+            {
+                if(process.HasExited)
+                    process = Process.Start(@"Old_World.exe");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
